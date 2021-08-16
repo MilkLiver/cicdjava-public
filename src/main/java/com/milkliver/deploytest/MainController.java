fe82c30476e2a7ce3f053e6829cc304741abd4b1
neil.yu <neil.yu@dynasafe.com.tw>
2021-08-16 17:36:40 +0800

    demo

--- src/main/java/com/milkliver/deploytest/MainController.java
+++ src/main/java/com/milkliver/deploytest/MainController.java
@@ -27,7 +27,7 @@ public class MainController {
 	// @Value("${version}")
 	// String version;
 
-	static String version = "v6.3.4";
+	static String version = "v6.3.5";
 
 	@Value("${environment}")
 	String environment;
