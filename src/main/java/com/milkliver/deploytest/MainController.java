9737b790c81a388c7fe4e49c4a2d763d5d8e88de
neil.yu <neil.yu@dynasafe.com.tw>
2021-10-29 16:21:34 +0800

    20211029-1621, change version to v6.7.2

--- src/main/java/com/milkliver/deploytest/MainController.java
+++ src/main/java/com/milkliver/deploytest/MainController.java
@@ -34,7 +34,7 @@ public class MainController {
 
 	private Counter requestCount;
 
-	static String version = "v6.7.1";
+	static String version = "v6.7.2";
 
 	static Map statusProbability = new HashedMap();
 
