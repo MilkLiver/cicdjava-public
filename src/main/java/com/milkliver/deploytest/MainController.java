647d4cb9833eeddffd22383f8298f89dcb3508af
neil.yu <neil.yu@dynasafe.com.tw>
2021-12-17 13:43:04 +0800

    20211217-1343-v6.13.4 update version

--- src/main/java/com/milkliver/deploytest/MainController.java
+++ src/main/java/com/milkliver/deploytest/MainController.java
@@ -38,7 +38,7 @@ public class MainController {
 
 	private Counter requestCount;
 
-	static String version = "v6.13.3";
+	static String version = "v6.13.4";
 
 //	static String mutateStr = "[{ \"op\": \"add\", \"path\": \"/metadata/labels/foo\", \"value\": \"bar\" }]";
 
