d7c0615bb95b90a2228a6b37c3dcc4c2d38742c9
neil.yu <neil.yu@dynasafe.com.tw>
2022-01-14 10:06:36 +0800

    20220114-v6.14.4 update version

--- src/main/java/com/milkliver/deploytest/MainController.java
+++ src/main/java/com/milkliver/deploytest/MainController.java
@@ -36,12 +36,12 @@ public class MainController {
 	// @Value("${version}")
 	// String version;
 
-	static final String showPage = "before";
-//	static final String showPage = "after";
+//	static final String showPage = "before";
+	static final String showPage = "after";
 
 	private Counter requestCount;
 
-	static String version = "v6.14.3";
+	static String version = "v6.14.4";
 
 //	static String mutateStr = "[{ \"op\": \"add\", \"path\": \"/metadata/labels/foo\", \"value\": \"bar\" }]";
 
